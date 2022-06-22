01f16046b20fa9eb252924fbacf2604dbde2a0ec
Michael <mgiano02@gmail.com>
2022-06-22 13:48:07 -0700

    Updated grid box sizes and made them responsive based on button prompt

--- scripts.js
+++ scripts.js
@@ -1,17 +1,15 @@
-// Selects container div
 const container = document.querySelector('#container');
 
 // Loop that creates grid
 function mkGrid() {
     for(let i = 1; i <= 16; i++){
-        let col = document.createElement('div');
-        container.appendChild(col);
-        col.classList.add('section');
+        let row = document.createElement('div');
+        container.appendChild(row);
+        row.classList.add('section');
         for (let j = 1; j <= 16; j++){
             let box = document.createElement('div');
             box.classList.add('grid');
-            box.innerText = '---------------';
-            col.appendChild(box);
+            row.appendChild(box);
         }
     }
 }
@@ -27,26 +25,33 @@ const hover = box.forEach(box => {
     });
 });
 
-// Add button popup
 const btn = document.querySelector('#btn');
 
 // Button popup event - Gives prompt
 btn.addEventListener('click', function(){
     let num = prompt("How many squares per side did you want to play with?", "16");
         if (num > 1 && num <= 100) {
-            remove();
-            // Make new grid and new hover
+            // Removes old grid
+            const allGrid = document.querySelectorAll('.section', '.grid');
+            allGrid.forEach(allGrid => {
+                allGrid.remove();
+            });
+            // Makes new grid
             for(let i = 1; i <= num; i++){
-                let col = document.createElement('div');
-                container.appendChild(col);
-                col.classList.add('section');
+                let row = document.createElement('div');
+                container.appendChild(row);
+                row.classList.add('section');
                 for (let j = 1; j <= num; j++){
                     let box = document.createElement('div');
                     box.classList.add('grid');
-                    box.innerText = '---------------';
-                    col.appendChild(box);
+                    row.appendChild(box);
+                    box.style.width = "100%";
+                    let dimensions = 500 / parseInt(num);
+                    box.style.height = `${dimensions}px`;
+                    box.style.width = `${dimensions}px`;
                 }
             }
+            // Add hover for new grid
             const box2 = document.querySelectorAll('.grid');
             box2.forEach(box => {
                 box.addEventListener('mouseover', function(){
@@ -57,22 +62,3 @@ btn.addEventListener('click', function(){
             alert("Please enter a suitable number!");
          }
 })
-
-const allGrid = document.querySelectorAll('.section', '.grid');
-
-// Function that removes the grid
-function remove() {
-    allGrid.forEach(allGrid => {
-        allGrid.remove();
-    });
-};
-
-
-// Next Steps
-// Make button event remove existing grid
-// - Function that removes div elements that make up the grid
-// -- Select all columns and grid box
-// -- Remove them
-    // Add new grid with new number of squares
-    // - Function that starts the loop again with enters nuumber of squares
-    // Confirm dimensions
